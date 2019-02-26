5c74927a42d6b189b64a024891f7a1ad0414d3de
Zainab1993 <46340200+Zainab1993@users.noreply.github.com>
2019-02-26 17:35:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 db: 'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/test',
 db_dev:'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/test'
+web:nodemon server.js
+web:npm start
