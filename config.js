b05983bb2bfa99b9d947633447b05d1b7c13223d
Zainab1993 <46340200+Zainab1993@users.noreply.github.com>
2019-02-27 14:41:21 +0500

    change db name from test to node-shop

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-db: 'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/test',
-db_dev:'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/test'
-web:nodemon server.js
-web:npm start
+db: 'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/node-shop',
+db_dev:'mongodb://aymenkhan092@gmail.com:aymen123#@localhost:27017/node-shop'
